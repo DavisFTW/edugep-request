7f668d25e89886435d201ee2ce237e3be516bc7c
Davis <davis@davis.com>
2023-01-30 12:43:54 +0200

    added a missing closing bracket

--- DBWorks.php
+++ DBWorks.php
@@ -4,11 +4,11 @@ function makeConnection(){
     $serverName = "localhost";
     $username = "username";
     $password = "password";
+    $databaseName = "DBNAME";
 
-    $conn = new mysqli($serverName, $username, $password);
+    $conn = new mysqli($serverName, $username, $password, $databaseName);
 
     if($conn->connect_error){
         die("Connection to DB failed !" . $conn->connect_error);
     }
 }
-?>
