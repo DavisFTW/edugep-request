77291383d13d3da8e134d4acb86812ad954bbca4
Linards Balega <kenheine1337@gmail.com>
2023-01-30 16:25:14 +0200

    finished forms

--- register.php
+++ register.php
@@ -10,22 +10,25 @@
 <body>
   <div class="container mt-5 d-flex justify-content-center p-4 rounded" id="registrationControl">
     <div class="col">
+      <div class="text-center">
+      <img class="mb-2" src="logo.png" alt="company's logo" width="300" height="75">
+      </div>
       <h2 class="text-center">User Registration</h2>
       <div class="mt-3">
         <form action="#">
           <div class="form-group row mt-2">
               <div class="col col-md-6" class="form-label">
                   <label class="form-label" for="first_name">First Name:</label>
-                  <input type="text" class="form-control" name="first_name" id="first_name" placeholder="Enter first name" required>
+                  <input type="text" class="form-control" name="first_name" id="first_name" placeholder="Enter your first name" required>
               </div>
               <div class="col col-md-6" class="form-label">
                   <label class="form-label" for="last_name">Last Name:</label>
-                  <input type="text" class="form-control" name="last_name" id="last_name" placeholder="Enter last name" required>
+                  <input type="text" class="form-control" name="last_name" id="last_name" placeholder="Enter your last name" required>
               </div>
           </div>
           <div class="form-group">
             <label for="email">Email:</label>
-            <input type="email" class="form-control" id="email" placeholder="Enter email" required>
+            <input type="email" class="form-control" id="email" placeholder="Enter your email" required>
           </div>
           <div class="form-group">
             <label for="pwd">Password:</label>
@@ -35,7 +38,16 @@
             <label for="pwd2">Confirm Password:</label>
             <input type="password" class="form-control" id="pwd2" placeholder="Confirm password" required>
           </div>
-          <button type="submit" class="btn btn-secondary">Submit</button>
+          <div class="row">
+            <div class="col">
+                <button type="submit" class="mt-2 btn btn-dark">Submit</button>
+            </div>
+            <div class="col col-md-3 offset-md-3">
+                <a href="login.php">
+                  <button type="button" class="mt-2 btn btn-secondary">Login</button>
+                </a>
+            </div>
+          </div>
         </form>
       </div>
     </div>
