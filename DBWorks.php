4763223cacc033011e8ef89f9055d9166848b3b2
DavisFTW <cheatingronald@gmail.com>
2023-02-01 13:44:35 +0000

    small changes

--- DBWorks.php
+++ DBWorks.php
@@ -4,16 +4,22 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST'){
     $last_name = $_POST['last_name'];
     $email = $_POST['email'];
     
+    if(verifyEmail($email)){
+        prompt("email already exists !");
+        header("Refresh:5");
+    }
+
     if ($_POST['pwd'] == $_POST['pwd2']) {
         $pwd = password_hash($_POST['pwd'], PASSWORD_DEFAULT);
         registerUser($first_name, $last_name, $email, $pwd);
         header('Location: login.php');
     } else {
-        header('Location: register_failed.php');
+        prompt("email already exists !");
+        header("Refresh:5");
     }
 }
 
-function verifyEmail($email){
+function verifyEmail($email){  # returns true if email is found !
     $conn = makeConnection();
 
     $query = "SELECT * FROM users WHERE email = '$email'";
@@ -53,6 +59,13 @@ function insertRequestData($equipment, $date, $returnDate){
 
     // insert the data
     closeConnection($conn);
+}
+
+function prompt($prompt_msg){
+    echo("<script type='text/javascript'> var answer = prompt('".$prompt_msg."'); </script>");
+
+    $answer = "<script type='text/javascript'> document.write(answer); </script>";
+    return($answer);
 }
 
 function registerUser($first_name, $last_name, $email, $password)
