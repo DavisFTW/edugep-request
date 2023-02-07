710949ebd40bb2be664b502803b93352e1715444
DavisFTW <cheatingronald@gmail.com>
2023-02-07 10:03:03 +0000

    latest version

--- DBWorks.php
+++ DBWorks.php
@@ -1,9 +1,4 @@
 <?php
-
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
 if ($_SERVER['REQUEST_METHOD'] == 'POST'){
     $first_name = $_POST['first_name'];
     $last_name = $_POST['last_name'];
